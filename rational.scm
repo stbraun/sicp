b92fadda8b9f7b79177193cbc18fd885dc2553e3
Stefan Braun <sb@stbraun.com>
2023-04-21 01:32:28 +0200

    Tests restructured.

--- rational.scm
+++ rational.scm
@@ -24,11 +24,31 @@
                   (else 
                     (cons (/ numerator g) (/ denominator g))))))
 
+        (module+ test
+                 (require rackunit)
+
+                 ; Create rationals
+                 (check-equal? (make-rat 1 2) (cons 1 2))
+                 (check-equal? (make-rat 3 6) (cons 1 2))
+                 (check-equal? (make-rat 3 6) (make-rat 1 2))
+                 (check-equal? (make-rat -1 -2) (cons 1 2))
+                 (check-equal? (make-rat 1 -2) (cons -1 2))
+                 (check-equal? (make-rat -1 2) (cons -1 2))
+                 )
+
         (define (numer r)
           (car r))
 
         (define (denom r)
           (cdr r))
+
+        (module+ test
+                 (define r1 (make-rat 2 5))
+                 (define r2 (make-rat 3 5))
+                 (define r3 (make-rat 1 3))
+
+                 (check-equal? (numer r1) 2)
+                 (check-equal? (denom r1) 5))
 
         (define (print-rat r)
           (display (numer r)) (display "/") (display (denom r)))
@@ -49,32 +69,7 @@
         (define (div-rat r1 r2)
           (make-rat (* (numer r1) (denom r2)) (* (numer r2) (denom r1))))
 
-        (define (equal-rat? r1 r2)
-          (= (* (numer r1) (denom r2)) (* (numer r2) (denom r1))))
-
         (module+ test
-                 (require rackunit)
-
-                 ; Create rationals
-                 (check-equal? (make-rat 1 2) (cons 1 2))
-                 (check-equal? (make-rat 3 6) (cons 1 2))
-                 (check-equal? (make-rat 3 6) (make-rat 1 2))
-                 (check-equal? (make-rat -1 -2) (cons 1 2))
-                 (check-equal? (make-rat 1 -2) (cons -1 2))
-                 (check-equal? (make-rat -1 2) (cons -1 2))
-
-                 (define r1 (make-rat 2 5))
-                 (define r2 (make-rat 3 5))
-                 (define r3 (make-rat 1 3))
-
-                 ; Compare for equality
-                 (check-true (equal-rat? r1 r1))
-                 (check-false (equal-rat? r1 r2))
-
-                 ; Extract components
-                 (check-equal? (numer (make-rat 1 2)) 1)
-                 (check-equal? (denom (make-rat 1 2)) 2)
-
                  ; Add
                  (check-equal? (add-rat r1 r2) (make-rat 1 1))
                  (check-equal? (add-rat r1 r3) (make-rat 11 15))
@@ -88,7 +83,13 @@
                  (check-equal? (mul-rat r1 r3) (make-rat 2 15))
 
                  ; Divide
-                 (check-equal? (div-rat r1 r3) (make-rat 6 5))
+                 (check-equal? (div-rat r1 r3) (make-rat 6 5)))
 
-                 )
+        (define (equal-rat? r1 r2)
+          (= (* (numer r1) (denom r2)) (* (numer r2) (denom r1))))
+
+        (module+ test
+                 ; Compare for equality
+                 (check-true (equal-rat? r1 r1))
+                 (check-false (equal-rat? r1 r2)))
         )
