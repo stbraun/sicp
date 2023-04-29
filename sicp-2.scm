b7adbd7b3ab01e810e433d26662c9a6a72550cf4
Stefan Braun <sb@stbraun.com>
2023-04-29 15:16:51 +0200

    Exercise 2.28 added.

--- sicp-2.scm
+++ sicp-2.scm
@@ -402,3 +402,55 @@
   (display (matrix-*-vector m v)) (newline)
   (display (transpose m)) (newline)
   (display (matrix-*-matrix m n)) (newline))
+
+; Exercise 2.38
+; The accumulate procedure is also known as fold-right, 
+; because it combines the first element of the sequence 
+; with the result of combining all the elements to the right.
+ is also a fold-left, which is similar to fold-right, 
+; except it combines elements working in the opposite direction.
+
+(define fold-right accumulate)
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+      result
+      (iter (op result (car rest)) (cdr rest))))
+  (iter initial sequence))
+
+(fold-right / 1 (list 1 2 3))  ; 3/2
+(/ 1 (fold-right / 1 (list 2 3)))
+(/ 1 (/ 2 (fold-right / 1 (list 3))))
+(/ 1 (/ 2 (/ 3 1)))
+(/ 1 (/ 2 3))
+(/ 3 2)
+
+(fold-left / 1 (list 1 2 3))   ; 1/6
+(fold-left / (/ 1 1) (list 2 3))
+(fold-left / (/ (/ 1 1) 2) (list 3))
+(fold-left / (/ (/ (/ 1 1) 2) 3) nil)
+(/ (/ (/ 1 1) 2) 3)
+(/ (/ 1 2) 3)
+(/ 1 (* 2 3))
+(/ 1 6)
+
+(fold-right list nil (list 1 2 3))
+(list 1 (fold-right list nil (list 2 3)))
+(list 1 (list 2 (fold-right list nil (list 3))))
+(list 1 (list 2 (list 3 nil)))
+
+(fold-left list nil (list 1 2 3))
+(fold-left list (list nil 1) (list 2 3))
+(fold-left list (list (list nil 1) 2) (list 3))
+(fold-left list (list (list (list nil 1) 2) 3) nil)
+(list (list (list nil 1) 2) 3)
+
+; fold-left and fold-right will produce the same result only
+; if op is associative.
+(fold-right + 111 (list 1 2 3))
+(fold-left + 111 (list 1 2 3))
+
+; ------------
+
+
