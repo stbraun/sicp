ae3fe106737102073f407b8b2e25c2683b174617
Stefan Braun <sb@stbraun.com>
2023-05-17 18:15:51 +0200

    tree->list and list->tree added.

--- set-tree.scm
+++ set-tree.scm
@@ -85,4 +85,31 @@
 (tree->list-1 tree-2)
 (tree->list-2 tree-2)
 
+; -------------
 
+; Exercise 2.64
+; The procedure list->tree converts an ordered list to a balanced binary tree.
+; The helper procedure partial-tree takes an integer n and a list of at least n elements 
+; and constructs a balanced tree containing the first n elements of the list.
+; The result returned by partial-tree is a pair whose car is the constructed tree 
+; and whose cdr is the list of elements not included in the tree.
+
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+    (cons '() elts)
+    (let ((left-size (quotient (- n 1) 2)))
+      (let ((left-result (partial-tree elts left-size)))
+        (let ((left-tree (car left-result))
+              (non-left-elts (cdr left-result))
+              (right-size (- n (+ left-size 1))))
+          (let ((this-entry (car non-left-elts))
+                (right-result (partial-tree (cdr non-left-elts) right-size)))
+            (let ((right-tree (car right-result))
+                  (remaining-elts (cdr right-result)))
+              (cons (make-tree this-entry left-tree right-tree)
+                    remaining-elts))))))))
+
+(list->tree '( 1 3 5 7 9))
