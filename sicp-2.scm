6e9dfe2ff8c38c7e035aedc4a33abca04ba38209
Stefan Braun <sb@stbraun.com>
2023-05-21 00:02:45 +0200

    lookup by key implemented.

--- sicp-2.scm
+++ sicp-2.scm
@@ -593,4 +593,20 @@
 
 ; ------------
 
+; Lookup a record by key given a set of records.
+
+(define (lookup given-key set-of-records)
+  (cond ((null? set-of-records) false)
+        ((equal? given-key (key (car set-of-records)))
+         (car set-of-records))
+        (else
+          (lookup given-key (cdr set-of-records)))))
+
+; Selector for a record's key.
+(define (key record)
+  (car record))
+
+(lookup 3 '(( 1 2 3) (3 4 5) (6 7 8)))
+(lookup 4 '(( 1 2 3) (3 4 5) (6 7 8)))
+
 
