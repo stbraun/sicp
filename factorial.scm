b1bc57c00c65a586906473ae2d1c8bbb0e2bf72e
Stefan Braun <sb@stbraun.com>
2025-03-31 16:17:46 +0200

    Tail-recursive variants of Fibonacci and Factorial added.

--- factorial.scm
+++ factorial.scm
@@ -1,6 +1,7 @@
 ; Some implementations of a factorial function.
 (module factorial racket
         (provide factorial-r
+                 factorial-t
                  factorial-i)
 
         ; Recursive approach.
@@ -12,6 +13,24 @@
             1
             (* n (factorial-r (- n 1)))))
 
+        (module+ test
+                 (require rackunit)
+                 (check-equal? 120 (factorial-r 5))
+                 (check-equal? 720 (factorial-r 6))
+                 (check-equal? 39916800 (factorial-r 11)))
+
+        ; Tail-recursive approach.
+        (define (factorial-t n)
+          (define (fact-iter product counter)
+            (if (> counter n)
+              product
+              (fact-iter (* counter product) (+ counter 1))))
+          (fact-iter 1 1))
+
+        (module+ test
+                 (check-equal? 120 (factorial-t 5))
+                 (check-equal? 720 (factorial-t 6))
+                 (check-equal? 39916800 (factorial-t 11)))
 
         ; Iterative approach
         ; The procedure is recursive, but the process is iterative: 
@@ -28,7 +47,7 @@
           (iter 1 1))
 
         (module+ test
-                 (require rackunit)
-                 (check-equal? 120 (factorial-r 5))
-                 (check-equal? 120 (factorial-i 5)))
+                 (check-equal? 120 (factorial-i 5))
+                 (check-equal? 720 (factorial-i 6))
+                 (check-equal? 39916800 (factorial-i 11)))
         )
--- fibonacci.scm
+++ fibonacci.scm
@@ -1,10 +1,10 @@
-; Tree recursion
+; Fibonacci number.
 (module fibonacci racket
         (provide fib
                  fib-2r
                  fib-i)
 
-        ; Fibonacci numbers
+        ; recursive approach
         (define (fib-2r n)
           (cond ((= n 0) 0)
                 ((= n 1) 1)
@@ -19,6 +19,24 @@
                  (check-equal? 3 (fib-2r 4))
                  (check-equal? 5 (fib-2r 5))
                  (check-equal? 55 (fib-2r 10)))
+
+        ; tail-recursive approach
+        (define (fib-tr n)
+          (define (fib-tail n a b)
+            (if (= n 0)
+              b
+              (fib-tail (- n 1) (+ a b) a)))
+          (fib-tail n 1 0))
+
+        (module+ test
+                 (require rackunit)
+                 (check-equal? 0 (fib-tr 0))
+                 (check-equal? 1 (fib-tr 1))
+                 (check-equal? 1 (fib-tr 2))
+                 (check-equal? 2 (fib-tr 3))
+                 (check-equal? 3 (fib-tr 4))
+                 (check-equal? 5 (fib-tr 5))
+                 (check-equal? 55 (fib-tr 10)))
 
         ; iterative approach
         (define (fib-i n)
