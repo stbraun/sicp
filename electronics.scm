2b4c4298423eb1ef60a1473d6db42ca7a63e1d6f
sb <sb@action.ms>
2026-05-26 14:25:35 +0200

    electronics: corner frequency added.

--- electronics.scm
+++ electronics.scm
@@ -3,6 +3,7 @@
 (module electronics racket
         (provide parR
                  serC
+                 corner-freq-order1
                  )
 (require "interval.scm")
 
@@ -17,5 +18,9 @@
   (div-interval
     (mul-interval c1 c2)
     (add-interval c1 c2)))
+
+;; Corner frequency of a first-order RC LP or HP filter
+(define (corner-freq-order1 R C)
+  (div-interval (make-interval 1 1) (mul-interval (make-interval 2 2) (mul-interval (make-interval pi pi) (mul-interval R C)))))
 
 ) ; close module electronics
